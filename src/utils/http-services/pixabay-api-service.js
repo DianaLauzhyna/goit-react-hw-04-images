a1ea1010b5d9d2fe5336a1162bc199309b5e3a60
Diana Lauzhyna <dlauzhyna@gmail.com>
2022-10-25 02:17:44 +0300

    chaanges

--- src/utils/http-services/pixabay-api-service.js
+++ src/utils/http-services/pixabay-api-service.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 const BASE_URL = 'https://pixabay.com';
-const API_KEY = '29404006-95afa3b6414bbb36dd662a5bf';
+const API_KEY = '30836512-07b4cd3b9ce810d91e506cec6';
 const IMG_TYPE = 'photo';
 const ORIENTATION = 'horizontal';
 const AGE_FILTER = 'true';
-console.log(axios.defaults);
+
 export const pixabayApiService = (searchQuery, page, IMG_PER_PAGE) => {
   axios.defaults.baseURL = BASE_URL;
-  return axios.get('api/', {
+  return axios
+    .get('api/', {
       params: {
         key: API_KEY,
         q: searchQuery,
@@ -17,7 +18,8 @@ export const pixabayApiService = (searchQuery, page, IMG_PER_PAGE) => {
         per_page: IMG_PER_PAGE,
         page: page,
       },
-    }).then(({ data }) => {
+    })
+    .then(({ data }) => {
       if (Array.isArray(data.hits)) {
         return data;
       } else {
